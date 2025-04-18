00f565219b3e2d00d4f2d7735c99f9c4688de98f
wala-sian <everything22092k3@gmail.com>
2025-04-19 00:10:34 +0700

    deploy

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,35 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+// import express from "express";
+// import mongoose from "mongoose";
+// import router from "./routes/Routes";
+// import { GeneticAlgorithm } from "./algorithms/GeneticAlgorithm";
+// import { AntColonyOptimization } from "./algorithms/AntColonyOptimization";
+
+// const app = express();
+// const port = process.env.PORT as string;
+// const mongoUri = process.env.MONGO_URI as string;
+
+// app.use(express.json());
+// app.use("/api", router);
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://admin:admin@fastship.l23v2.mongodb.net/FastShip?retryWrites=true&w=majority&appName=FastShip"
+//   )
+//   .then(() => {
+//     app.listen(3000, () => {
+//       console.log("server running on port" + port);
+//     });
+//   });
+
+// const ga = new GeneticAlgorithm();
+// const initialRoute = ga.run();
+
+// const aco = new AntColonyOptimization();
+// const optimizedRoute = aco.run(initialRoute);
+// console.log("Lộ trình tối ưu:", optimizedRoute);
+
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
@@ -5,10 +37,18 @@ import mongoose from "mongoose";
 import router from "./routes/Routes";
 import { GeneticAlgorithm } from "./algorithms/GeneticAlgorithm";
 import { AntColonyOptimization } from "./algorithms/AntColonyOptimization";
+import cors from "cors";
 
 const app = express();
-const port = process.env.PORT as string;
-const mongoUri = process.env.MONGO_URI as string;
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://fastship-be.onrender.com"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/api", router);
@@ -18,14 +58,17 @@ mongoose
     "mongodb+srv://admin:admin@fastship.l23v2.mongodb.net/FastShip?retryWrites=true&w=majority&appName=FastShip"
   )
   .then(() => {
-    app.listen(3000, () => {
-      console.log("server running on port" + port);
-    });
+    app.listen(3000, () => {});
+  })
+  .catch((err) => {
+    process.exit(1);
   });
 
-const ga = new GeneticAlgorithm();
-const initialRoute = ga.run();
+setTimeout(() => {
+  const ga = new GeneticAlgorithm();
+  const initialRoute = ga.run();
 
-const aco = new AntColonyOptimization();
-const optimizedRoute = aco.run(initialRoute);
-console.log("Lộ trình tối ưu:", optimizedRoute);
+  const aco = new AntColonyOptimization();
+  const optimizedRoute = aco.run(initialRoute);
+  console.log("Lộ trình tối ưu:", optimizedRoute);
+}, 1000);
