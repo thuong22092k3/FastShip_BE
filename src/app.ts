26c0527bba92ac0c3336b803f3c8d7ca9849885e
wala-sian <everything22092k3@gmail.com>
2025-05-27 00:34:14 +0700

    Fix | CORS

--- src/app.ts
+++ src/app.ts
@@ -41,14 +41,14 @@ import cors from "cors";
 
 const app = express();
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "https://fastship-be.onrender.com"],
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials: true,
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://fastship-be.onrender.com"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/api", router);
