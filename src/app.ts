bc30749a8a230790130acd6179a4b0329ea1d051
wala-sian <everything22092k3@gmail.com>
2025-06-07 09:16:57 +0700

    Fix | optimization controller

--- src/app.ts
+++ src/app.ts
@@ -30,14 +30,16 @@
 // const optimizedRoute = aco.run(initialRoute);
 // console.log("Lộ trình tối ưu:", optimizedRoute);
 
+import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
-import router from "./routes/Routes";
-import { GeneticAlgorithm } from "./algorithms/GeneticAlgorithm";
 import { AntColonyOptimization } from "./algorithms/AntColonyOptimization";
-import cors from "cors";
+import { GeneticAlgorithm } from "./algorithms/GeneticAlgorithm";
+import { createDistanceMatrix } from "./algorithms/KhoangCach";
+import DiaDiemModel from "./models/DiaDiem";
+import router from "./routes/Routes";
+dotenv.config();
 
 const app = express();
 
@@ -57,20 +59,38 @@ mongoose
   .connect(
     "mongodb+srv://admin:admin@fastship.l23v2.mongodb.net/FastShip?retryWrites=true&w=majority&appName=FastShip"
   )
-  .then(() => {
+  .then(async () => {
     app.listen(5000, () => {
       console.log("server running on port" + 5000);
     });
+    try {
+      const locations = await DiaDiemModel.find().lean();
+      if (!locations || locations.length === 0) {
+        console.error("Không tìm thấy dữ liệu locations");
+        return;
+      }
+
+      const distanceMatrix = createDistanceMatrix(locations);
+
+      const ga = new GeneticAlgorithm(locations, distanceMatrix);
+      const initialRoute = ga.run();
+
+      const aco = new AntColonyOptimization(locations, distanceMatrix);
+      const optimizedRoute = aco.run(initialRoute);
+      console.log("Lộ trình tối ưu:", optimizedRoute);
+    } catch (err) {
+      console.error("Lỗi khi chạy thuật toán:", err);
+    }
   })
   .catch((err) => {
     process.exit(1);
   });
 
-setTimeout(() => {
-  const ga = new GeneticAlgorithm();
-  const initialRoute = ga.run();
+// setTimeout(() => {
+//   const ga = new GeneticAlgorithm();
+//   const initialRoute = ga.run();
 
-  const aco = new AntColonyOptimization();
-  const optimizedRoute = aco.run(initialRoute);
-  console.log("Lộ trình tối ưu:", optimizedRoute);
-}, 1000);
+//   const aco = new AntColonyOptimization();
+//   const optimizedRoute = aco.run(initialRoute);
+//   console.log("Lộ trình tối ưu:", optimizedRoute);
+// }, 1000);
