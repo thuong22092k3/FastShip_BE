a168f148784668a2e5edd1875e26a699ee3d9894
wala-sian <everything22092k3@gmail.com>
2025-06-08 02:25:47 +0700

    Fix | optimization controller

--- src/algorithms/Data.ts
+++ src/algorithms/Data.ts
@@ -4,6 +4,8 @@ export const locations: IDiaDiem[] = [
     DiaDiemId: "DD001",
     name: "Kho Hàng",
     address: "ktx khu A, Tạ Quang Bửu, Linh trung, thủ đức, thành phố HCM",
+    district: "Thủ đức",
+    province: "TP HCM",
     latitude: 10.7769,
     longitude: 106.7009,
   },
@@ -12,6 +14,8 @@ export const locations: IDiaDiem[] = [
     name: "Hà Nội",
     address: "báck khoa, Hoàn kiếm, Hà nội",
     latitude: 10.776,
+    district: "Thủ đức",
+    province: "TP HCM",
     longitude: 106.705,
   },
   {
@@ -20,6 +24,8 @@ export const locations: IDiaDiem[] = [
     address: "ktx khu A, Tạ Quang Bửu, Linh trung, Quận 1, thành phố HCM",
     latitude: 10.78,
     longitude: 106.71,
+    district: "Thủ đức",
+    province: "TP HCM",
   },
   {
     DiaDiemId: "DD004",
@@ -27,5 +33,7 @@ export const locations: IDiaDiem[] = [
     address: "Đà nẵng",
     latitude: 10.784,
     longitude: 106.72,
+    district: "Thủ đức",
+    province: "TP HCM",
   },
 ];
